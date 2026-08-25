872f121cd5ec6a616316d9afbe178f4dc217335f
Jan Tomický <jan@tomicky.cz>
2026-08-25 12:53:59 +0200

    fix: ship prebuilt assets, drop consumer npm build step

---
+++ public/build/974afbc0-6549-4da0-82ce-84186e146bcd/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.portadesign_data_quality_bundle = "/bundles/portadesigndataquality/build/974afbc0-6549-4da0-82ce-84186e146bcd/static/js/remoteEntry.js"
+
+      
+    
